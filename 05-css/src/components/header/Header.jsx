317b6f773c664a1158cdb5ae307c550d6a4bfa8a
Rohit Dahiya <rohitdahiya2802@gmail.com>
2026-08-11 16:07:41 +0530

    Added style and css

---
+++ 05-css/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={style.header}>
+      <h3>Rohit Dahiya</h3>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Header
